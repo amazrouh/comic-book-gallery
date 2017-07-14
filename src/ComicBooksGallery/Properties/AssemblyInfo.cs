116533924a9372a15d214b0c4f6abbcab039ee17
maz <amazrouh@gmail,com>
2017-07-14 19:33:54 +0200

    2nd initial commit

--- src/ComicBooksGallery/Properties/AssemblyInfo.cs
+++ src/ComicBooksGallery/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComicBookGallery")]
+[assembly: AssemblyTitle("ComicBooksGallery")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComicBookGallery")]
+[assembly: AssemblyProduct("ComicBooksGallery")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a73eb42-587f-413b-95a4-bc9bad368826")]
+[assembly: Guid("1914a777-0c62-4ddf-992d-77a2f0bfdc7c")]
 
 // Version information for an assembly consists of the following four values:
 //
